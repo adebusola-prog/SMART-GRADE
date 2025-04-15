6cf86c49e3b575a8339a152bd15dea0d6f684cf6
Adebusola Adeyeye <aadeyeye@afexnigeria.com>
2025-04-15 21:32:14 +0300

    added procfile

--- smart_grade/settings.py
+++ smart_grade/settings.py
@@ -131,22 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'smart_grade' / 'static',
-     ]
-
-
-STATIC_ROOT = BASE_DIR / 'static'
-
-# STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / "static"
+    BASE_DIR / 'smart_grade' / 'static', 
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
